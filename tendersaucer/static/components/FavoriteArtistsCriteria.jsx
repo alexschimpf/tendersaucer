74da09e0667cafcc5bb3145c6b40a84b61538249
Alex Schimpf <schimpf1@illinois.edu>
2019-04-16 21:21:12 -0700

    Fixed some typos. Added to todo.

--- tendersaucer/static/components/FavoriteArtistsCriteria.jsx
+++ tendersaucer/static/components/FavoriteArtistsCriteria.jsx
@@ -109,9 +109,9 @@ class FavoriteArtistsCriteria extends React.Component {
                     <h3 className="adventurousness-param-header">Adventurousness</h3>
                     <InfoIcon className="adventurousness-info-icon"
                         message="This scale represents how much you want your playlist to
-                                 deviate from your favorite artists. 0 means you only want
+                                 deviate from your favorite artists. 0 means you only want to
                                  listen to artists you know. 3 means you're willing to listen
-                                 to artists who are more different than your favorite artists." />
+                                 to artists who are a lot different than your favorite artists." />
                     <SliderWithTooltip className="slider" defaultValue={0} min={0} max={3}
                         handleStyle={{borderColor: 'salmon'}}
                         trackStyle={{backgroundColor: 'salmon'}}
